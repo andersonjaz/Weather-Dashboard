7939ac44aeca7b1e6d770b825451f72b6e284d4b
Laura Jazmin Anderson <jazminandersonxo@gmail.com>
2021-12-11 15:57:27 -0800

    initial commit

---
+++ assets/js/weather-script.js
@@ -0,0 +1,143 @@
+var listCities = [];;
+var apiKey = "59b8a8c405ff8423f99a6f8cdaf39e42";
+
+
+//save city searches in storage
+function storeCities() {
+    localStorage.setItem("cities", JSON.stringify(listCities));
+}
+
+function createCityList(){
+    $(".listCities").empty();
+    listCities.forEach(function(city) {
+        $(".listCities").prepend($(`<button class="list-group-item list-group-item-action cityButton" data-city="${city}">${city}</button>`));
+    })
+}
+
+//load api for specific city and lists city
+function init() {
+    var storedCities = JSON.parse(localStorage.getItem("cities"));
+
+    if (storedCities !== null) {
+        listCities = storedCities;
+    }
+
+    createCityList();
+
+    if (listCities) {
+        var thisCity = listCities[listCities.length - 1]
+        getCurrentWeather(thisCity, apiKey);
+        getForecast(thisCity, apiKey);
+    }
+}
+
+//api for current city
+function getCurrentWeather(thisCity, apiKey) {
+    var currentURL = `https://api.openweathermap.org/data/2.5/weather?q=${thisCity}&units=metric&appid=${apiKey}`;
+    var cityLat;
+    var cityLong;
+
+    $.ajax({
+        url: currentURL,
+        method: "GET"
+    }).then(function (data) {
+        $(".cityCurrent").append(
+            `<div class="row ml-1">
+                <h3 class="mr-3">${data.name} (${(new Date(1000 * data.dt).getUTCDate()) - 1}/${(new Date(1000 * data.dt).getUTCMonth()) + 1}/${new Date(1000 * data.dt).getUTCFullYear()})</h3>
+                <img src="http://openweathermap.org/img/w/${data.weather[0].icon}.png">
+            </div>`
+        )
+        $(".cityCurrent").append(`<p>Temperature: ${data.main.temp} &degC</p>`)
+        $(".cityCurrent").append(`<p>Humidity: ${data.main.humidity} %</p>`)
+        $(".cityCurrent").append(`<p>Wind: ${data.wind.speed} Kph</p>`)
+        cityLat = data.coord.lat;
+        cityLong = data.coord.lon;
+        getUVI(apiKey, cityLat, cityLong);
+    })
+
+}
+
+
+//get uvi for recent city
+function getUVI(apiKey, cityLat, cityLong) {
+    var uvURL = `https://api.openweathermap.org/data/2.5/uvi?lat=${cityLat}&lon=${cityLong}&appid=${apiKey}`;
+
+    $.ajax({
+        url: uvURL,
+        method: "GET"
+    }).then(function (data) {
+        $(".cityCurrent").append(`<p>UV Index: <span class="badge badge-warning p-2">${data.value}</span></p>`);
+    })
+}
+
+
+//getting forecast for recent city
+
+function getForecast(thisCity, apiKey) {
+    
+    var forecastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${thisCity}&units=metric&appid=${apiKey}`;
+
+    $.ajax({
+        url: forecastURL,
+        method: "GET"
+    }).then(function (data) {
+        
+        for (i = 0; i < data.list.length; i++) {
+            if (data.list[i].dt_txt.search("18:00:00") != -1) {
+                var forecastDate = data.list[i];
+                $(".forecast").append(
+                    `<div class="card bg-primary shadow m-4">
+                        <div class="card-body">
+                            <h4 class="card-title">${new Date(1000 * forecastDate.dt).getUTCDate()}/${(new Date(1000 * forecastDate.dt).getUTCMonth()) + 1}/${new Date(1000 * forecastDate.dt).getUTCFullYear()}</h4>
+                            <div class="card-text">
+                                <img src="http://openweathermap.org/img/w/${forecastDate.weather[0].icon}.png">
+                                <p class="card-text">Temp: ${forecastDate.main.temp} &degC</p>
+                                <p class="card-text">Humidity: ${forecastDate.main.humidity} %</p>
+                            </div>
+                        </div>
+                    </div>`
+                );
+            }
+        }
+
+    })
+}
+
+//retrieve weather and forecast
+
+function displayCityWeather() {
+    var thisCity = $(this).attr("data-city");
+
+    $(".cityCurrent").empty();
+    getCurrentWeather(thisCity, apiKey);
+
+    $(".forecast").empty();
+    getForecast(thisCity, apiKey);
+    
+}
+ 
+
+$("form").on("submit", function(event) {
+    event.preventDefault();
+    var newCity = $("#citySearch").val().trim();
+    listCities.push(newCity);
+    createCityList();
+    storeCities();
+    $("#citySearch").val("");
+    //displayCityWeather();
+})
+
+
+//click to display the City
+
+$(".listCities").on("click", ".cityButton", displayCityWeather);
+
+
+// Button to clear the city list from local storage
+
+var clear = document.querySelector("#clearCities");
+
+clear.addEventListener("click", function () {
+    localStorage.clear();
+    location.reload();
+});
